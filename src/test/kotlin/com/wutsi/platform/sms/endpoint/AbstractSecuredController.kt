0c74df9eef39306a16d0180b08d0c162ea2515e5
wutsi <herve.tchepannou@wutsi.com>
2021-12-23 17:17:02 -0500

    fix tests

--- src/test/kotlin/com/wutsi/platform/sms/endpoint/AbstractSecuredController.kt
+++ src/test/kotlin/com/wutsi/platform/sms/endpoint/AbstractSecuredController.kt
@@ -1,9 +1,5 @@
 package com.wutsi.platform.sms.endpoint
 
-import com.auth0.jwt.interfaces.RSAKeyProvider
-import com.nhaarman.mockitokotlin2.any
-import com.nhaarman.mockitokotlin2.doReturn
-import com.nhaarman.mockitokotlin2.whenever
 import com.wutsi.platform.core.security.SubjectType
 import com.wutsi.platform.core.security.SubjectType.USER
 import com.wutsi.platform.core.security.spring.SpringAuthorizationRequestInterceptor
@@ -11,34 +7,12 @@ import com.wutsi.platform.core.security.spring.jwt.JWTBuilder
 import com.wutsi.platform.core.test.TestRSAKeyProvider
 import com.wutsi.platform.core.test.TestTokenProvider
 import com.wutsi.platform.core.test.TestTracingContext
-import com.wutsi.platform.core.tracing.TracingContext
 import com.wutsi.platform.core.tracing.spring.SpringTracingRequestInterceptor
 import com.wutsi.platform.core.util.URN
-import com.wutsi.platform.security.WutsiSecurityApi
-import com.wutsi.platform.security.dto.GetKeyResponse
-import com.wutsi.platform.security.dto.Key
-import org.junit.jupiter.api.BeforeEach
-import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.web.client.RestTemplate
-import java.util.Base64
 
 abstract class AbstractSecuredController {
-    private lateinit var keyProvider: RSAKeyProvider
-    private lateinit var tracingContext: TracingContext
-
-    @MockBean
-    lateinit var securityApi: WutsiSecurityApi
-
-    @BeforeEach
     open fun setUp() {
-        tracingContext = TestTracingContext()
-        keyProvider = TestRSAKeyProvider()
-
-        val key = Key(
-            algorithm = "RSA",
-            content = Base64.getEncoder().encodeToString(keyProvider.getPublicKeyById("1").encoded)
-        )
-        doReturn(GetKeyResponse(key)).whenever(securityApi).getKey(any())
     }
 
     protected fun createResTemplate(
@@ -54,13 +28,13 @@ abstract class AbstractSecuredController {
                 subject = subjectId.toString(),
                 subjectType = subjectType,
                 scope = scope,
-                keyProvider = keyProvider,
+                keyProvider = TestRSAKeyProvider(),
                 admin = admin,
                 name = URN.of("user", subjectId.toString()).value,
             ).build()
         )
 
-        rest.interceptors.add(SpringTracingRequestInterceptor(tracingContext))
+        rest.interceptors.add(SpringTracingRequestInterceptor(TestTracingContext()))
         rest.interceptors.add(SpringAuthorizationRequestInterceptor(tokenProvider))
         return rest
     }
